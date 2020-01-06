c56c72bffb0a6daceef61e59ec5dcd378c7fbb39
Jannik Silvanus <jasilvan@users.noreply.github.com>
2020-01-06 12:46:28 +0100

    p10k: Disable git symbol for better windows compat

--- .p10k.zsh
+++ .p10k.zsh
@@ -336,7 +336,9 @@
   #####################################[ vcs: git status ]######################################
   # Branch icon. Set this parameter to '\uF126 ' for the popular Powerline branch icon.
   # typeset -g POWERLEVEL9K_VCS_BRANCH_ICON='\uF126 '
-  typeset -g POWERLEVEL9K_VCS_BRANCH_ICON='\uF126 '
+  # TODO: Find a font/symbol combination that works on windows...
+  # typeset -g POWERLEVEL9K_VCS_BRANCH_ICON='\uF126 '
+  typeset -g POWERLEVEL9K_VCS_BRANCH_ICON=' '
 
   # Untracked files icon. It's really a question mark, your font isn't broken.
   # Change the value of this parameter to show a different icon.
