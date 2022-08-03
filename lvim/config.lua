a8e06b847a02a28356fcb9037f449f364ad045e9
Jannik Silvanus <jasilvanus@users.noreply.github.com>
2022-08-03 12:42:14 +0200

    lvim: avoid scrolling upon buffer switching

--- lvim/config.lua
+++ lvim/config.lua
@@ -276,6 +276,37 @@ vim.cmd([[
   autocmd FocusGained * checktime
 ]])
 
+-- Avoid repositioning of current line when switching buffers, as it makes
+-- e.g. visually comparing files very difficult
+vim.cmd([[
+" Save current view settings on a per-window, per-buffer basis.
+function! AutoSaveWinView()
+    if !exists("w:SavedBufView")
+        let w:SavedBufView = {}
+    endif
+    let w:SavedBufView[bufnr("%")] = winsaveview()
+endfunction
+
+" Restore current view settings.
+function! AutoRestoreWinView()
+    let buf = bufnr("%")
+    if exists("w:SavedBufView") && has_key(w:SavedBufView, buf)
+        let v = winsaveview()
+        let atStartOfFile = v.lnum == 1 && v.col == 0
+        if atStartOfFile && !&diff
+            call winrestview(w:SavedBufView[buf])
+        endif
+        unlet w:SavedBufView[buf]
+    endif
+endfunction
+
+" When switching buffers, preserve window view.
+if v:version >= 700
+    autocmd BufLeave * call AutoSaveWinView()
+    autocmd BufEnter * call AutoRestoreWinView()
+endif
+]])
+
 -- Prepare xml syntax folding -- enable with 'set foldmethod=syntax' (ensure syntax on before)
 vim.cmd('let g:xml_syntax_folding=1')
 
