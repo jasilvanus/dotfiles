e273ede74aca7310c1a8c94422358681fb75c45b
Jannik Silvanus <jasilvanus@users.noreply.github.com>
2022-09-06 09:28:13 +0200

    lvim: add illuminate plugin
    This highlights the current word under cursor in the whole buffer
    interactively.
    LunarVim already brings this functionality, but only for filetypes with
    LSP support.
    
    This plugin supports multiple data sources, including simple separator-based
    word detection. This is useful to interactively highlight in LLVM IR.

--- lvim/config.lua
+++ lvim/config.lua
@@ -156,6 +156,49 @@ Setup_null_ls()
 -- end
 -- Setup_bufferline()
 
+function Setup_illuminate()
+  require('illuminate').configure({
+    -- providers: provider used to get references in the buffer, ordered by priority
+    providers = {
+        'lsp',
+        'treesitter',
+        'regex',
+    },
+    -- delay: delay in milliseconds
+    delay = 100,
+    -- filetype_overrides: filetype specific overrides.
+    -- The keys are strings to represent the filetype while the values are tables that
+    -- supports the same keys passed to .configure except for filetypes_denylist and filetypes_allowlist
+    filetype_overrides = {},
+    -- filetypes_denylist: filetypes to not illuminate, this overrides filetypes_allowlist
+    filetypes_denylist = {
+        'dirvish',
+        'fugitive',
+    },
+    -- filetypes_allowlist: filetypes to illuminate, this is overriden by filetypes_denylist
+    filetypes_allowlist = { 'llvm' },
+    -- modes_denylist: modes to not illuminate, this overrides modes_allowlist
+    modes_denylist = {},
+    -- modes_allowlist: modes to illuminate, this is overriden by modes_denylist
+    modes_allowlist = {},
+    -- providers_regex_syntax_denylist: syntax to not illuminate, this overrides providers_regex_syntax_allowlist
+    -- Only applies to the 'regex' provider
+    -- Use :echom synIDattr(synIDtrans(synID(line('.'), col('.'), 1)), 'name')
+    providers_regex_syntax_denylist = {},
+    -- providers_regex_syntax_allowlist: syntax to illuminate, this is overriden by providers_regex_syntax_denylist
+    -- Only applies to the 'regex' provider
+    -- Use :echom synIDattr(synIDtrans(synID(line('.'), col('.'), 1)), 'name')
+    providers_regex_syntax_allowlist = {},
+    -- under_cursor: whether or not to illuminate under the cursor
+    under_cursor = true,
+  })
+    -- Override highlighting style, copy LSP style
+    vim.cmd([[
+    hi clear illuminatedWord
+    hi link illuminatedWord LspReferenceText
+  ]])
+  end
+Setup_illuminate()
 
 -- Virtual text enable/disable/toggle functions
 -- I could not find a way to query the current state,
@@ -396,6 +439,7 @@ lvim.plugins = {
     {"tpope/vim-fugitive"},
     {"rhysd/vim-llvm"},
     {"nvim-telescope/telescope-project.nvim"},
+    {"RRethy/vim-illuminate"},
     -- {"folke/tokyonight.nvim"},
     -- {
     --   "folke/trouble.nvim",
@@ -407,6 +451,17 @@ lvim.plugins = {
 lvim.autocommands.custom_groups = {
   -- { "BufWinEnter", "*.lua", "setlocal ts=8 sw=8" },
   { "BufWinEnter", "*.ll", ":lua vim.bo.commentstring = '; %s'" },
+  { "BufWinEnter", "*.dxil", "set filetype=llvm" },
   { "BufWinEnter", "*.cpp", "setlocal colorcolumn=120" },
   { "BufWinEnter", "*.h", "setlocal colorcolumn=120" },
 }
+
+-- The highlight link is a hack to get it to work, setting in the config
+-- function does not work for some reason. I only need it for llvm for the time being.
+vim.cmd([[
+autocmd FileType llvm set iskeyword+=%
+autocmd FileType llvm set iskeyword+=!
+autocmd FileType llvm set iskeyword+=.
+autocmd FileType llvm set iskeyword+=#
+autocmd FileType llvm hi link illuminatedWord LspReferenceText
+]])
